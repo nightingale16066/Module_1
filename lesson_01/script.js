13b18720523fbc490e7c5021e61b367822de0c2b
nightingale16066 <nightingale16066@gmail.com>
2022-08-17 12:24:05 +0300

    Add second good

--- lesson_01/script.js
+++ lesson_01/script.js
@@ -1,10 +1,21 @@
 'use strict';
 
-const goodName = 'Samsung Galaxy Buds 2';
-const goodAmount = 4;
-const goodCategory = 'Headphones';
-const goodPrice = 10000;
+{
+    const goodName = 'Samsung Galaxy Buds 2';
+    const goodAmount = 4;
+    const goodCategory = 'Headphones';
+    const goodPrice = 10000;
 
-console.log('goodName: ', goodName);
+    console.log('goodName: ', goodName);
+    console.log(`Total cost: ${goodAmount * goodPrice}`);
+}
 
-console.log(`Total cost: ${goodAmount * goodPrice}`);
+{
+    const goodName = 'Samsung Galaxy Note 10';
+    const goodAmount = 2;
+    const goodCategory = 'Смартфон';
+    const goodPrice = 49990;
+
+    console.log('goodName: ', goodName);
+    console.log(`Total cost: ${goodAmount * goodPrice}`);
+}
