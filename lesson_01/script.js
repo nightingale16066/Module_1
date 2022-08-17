9af21defa8cb7c05a70ed08b6cab466714337b3a
nightingale16066 <nightingale16066@gmail.com>
2022-08-17 11:58:01 +0300

    Add lesson_01

---
+++ lesson_01/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const goodName = 'Samsung Galaxy Buds 2';
+const goodAmount = 4;
+const goodCategory = 'Headphones';
+const goodPrice = 10000;
+
+console.log('goodName: ', goodName);
+
+console.log(`Total cost: ${goodAmount * goodPrice}`);
