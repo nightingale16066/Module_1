32b5f9a18b5cf1e1f143f865d8e71da9168f65eb
nightingale16066 <nightingale16066@gmail.com>
2022-08-22 13:07:30 +0300

    Lesson_11: Eslint improvements

--- lesson_10/cart.js
+++ lesson_10/cart.js
@@ -1,46 +1,45 @@
 'use strict';
 
 const cart = {
-    items: [],
-    count: 0,
+  items: [],
+  count: 0,
 
-    get totalPrice() {
-        return this.calculateItemPrice();
-    },
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
 
-    add(name, price, amount = 1) {
-        this.items.push({
-            name,
-            price,
-            amount,
-        });
-        this.increaseCount(amount);
-    },
+  add(name, price, amount = 1) {
+    this.items.push({
+      name,
+      price,
+      amount,
+    });
+    this.increaseCount(amount);
+  },
 
-    increaseCount(number) {
-        this.count += number;
-    },
+  increaseCount(number) {
+    this.count += number;
+  },
 
-    calculateItemPrice() {
-        return this.items.reduce((sum, {price, amount}) => {
-            return sum += price * amount;
-        }, 0)
-    },
+  calculateItemPrice() {
+    return this.items.reduce(
+      (sum, {price, amount}) => sum += price * amount, 0);
+  },
 
-    clear() {
-        this.items = [];
-        this.count = 0;
-    },
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
 
-    print() {
-        console.log(JSON.stringify(this.items));
-        console.log(this.totalPrice);
-    },
-}
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(this.totalPrice);
+  },
+};
 
 cart.add('Notebook', 150, 5);
 cart.add('Pen', 10);
-cart.add("Lolipop", 25, 10);
+cart.add('Lolipop', 25, 10);
 cart.print();
 
 
